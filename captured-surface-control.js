4763120b58115a87b20e0c93cbef0a5d9b3e4276
Elad Alon <eladalon@google.com>
2023-07-04 14:01:36 +0200

    Seed

---
+++ captured-surface-control.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+  group: "cg/sccg",
+  specStatus: "CG-DRAFT",
+  github: {
+    repoURL: "https://github.com/screen-share/captured-surface-control",
+    branch: "master",
+  },
+  editors: [
+    {
+      name: "Elad Alon",
+      email: "eladalon@google.com",
+      company: "Google",
+    },
+  ],
+  xref: [
+    "html",
+    "infra",
+    "permissions",
+    "permissions-policy",
+    "dom",
+    "mediacapture-streams",
+    "webidl",
+    "screen-capture",
+  ],
+  shortName: "captured-surface-control",
+  subjectPrefix: "[captured-surface-control]",
+};
